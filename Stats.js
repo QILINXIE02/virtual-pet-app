2bbf661e7205995f602cb025012b5b010d68b751
qilinxie02 <qxie@gicw.org>
2024-08-12 13:30:21 -0700

    removes files

--- Stats.js
+++
@@ -1,9 +0,0 @@
-export default function Stats({ pet }) {
-  return `
-    <div id="stats">
-      <p>Happiness: ${pet.happiness}</p>
-      <p>Hunger: ${pet.hunger}</p>
-      <p>Energy: ${pet.energy}</p>
-    </div>
-  `;
-}
